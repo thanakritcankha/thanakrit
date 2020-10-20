a160ceb3cb2c05426d68e3084522ef81d56f5d7b
Thanakrit <thanakrit.cankha@mail.kmutt.ac.th>
2020-10-20 15:19:20 +0700

    First time

---
+++ src/gittest/Gittest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author WAVE-TANAKI
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    
+}
